f1c840a7b02cd3741767832c28a13eb0f6ab0904
Beytullahzor7 <beytullahzor7@gmail.com>
2021-12-24 14:39:16 +0300

    Refactored

--- src/OtherSubjects/MathClass.java
+++ src/OtherSubjects/MathClass.java
@@ -1,8 +1,14 @@
 package OtherSubjects;
 
+import java.util.Random;
+
 public class MathClass {
     public static void main(String[] args) {
         int a = (int)(Math.random()*11);
         System.out.println(a);
+
+        Random rand = new Random();
+        int b = rand.nextInt(10);
+        System.out.println(b);
     }
 }
