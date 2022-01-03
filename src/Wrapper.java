13d33e79f5c5e4559d8b3ce7449d7e0ff530821a
Beytullahzor7 <beytullahzor7@gmail.com>
2022-01-03 21:23:24 +0300

    WrapperClass

---
+++ src/Wrapper.java
@@ -0,0 +1,15 @@
+public class Wrapper {
+    public static void main(String[] args) {
+        Integer a = 20;
+        String b = a.toString();
+        String g = "ad0";
+
+        //System.out.println(b+g);
+
+        String c = "30";
+        int d = 5;
+        int f = Integer.parseInt(c);
+        //System.out.println(Integer.parseInt(c) + d);
+        System.out.println(d+f);
+    }
+}
