56945dbf7bddb277708f1c1443bc2a25714bd95b
Beytullahzor7 <beytullahzor7@gmail.com>
2022-01-04 23:30:35 +0300

    Generic Updated

---
+++ src/Generic/IDatabase.java
@@ -0,0 +1,13 @@
+package Generic;
+
+public interface IDatabase <T> {
+
+    public boolean insert(T data);
+
+    public boolean delete(T data);
+
+    public boolean update(T data);
+
+    public T select();
+
+}
---
+++ src/Generic/Main3.java
@@ -0,0 +1,10 @@
+package Generic;
+
+public class Main3 {
+    public static void main(String[] args) {
+
+        Student<Integer> stu = new Student<>();
+        stu.insert(15);
+
+    }
+}
