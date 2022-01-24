c5909f98d37dad166005581780fa74602e6a2210
Beytullahzor7 <beytullahzor7@gmail.com>
2022-01-24 18:26:54 +0300

    Updated

--- src/IO/InputByteArray.java
+++ src/IO/InputByteArray.java
@@ -7,7 +7,7 @@ public class InputByteArray {
     public static void main(String[] args) {
         byte[] dizi = {1,2,3,12,24,32,66,73};
 
-        ByteArrayInputStream input = new ByteArrayInputStream(dizi, 2,6);
+        ByteArrayInputStream input = new ByteArrayInputStream(dizi, 2,4);
         System.out.println("Kullanılabilen Byte Sayisi: " + input.available());
 
         int i = input.read();
