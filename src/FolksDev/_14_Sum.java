9b2abd72dd21c388e97ae237f1a346a1b70f3d9f
Beytullahzor7 <beytullahzor7@gmail.com>
2022-03-10 20:35:25 +0300

    Updated

---
+++ src/FolksDev/_14_Sum.java
@@ -0,0 +1,13 @@
+package FolksDev;
+
+public class _14_Sum {
+    public static void main(String[] args) {
+        int n = 0;
+        System.out.print(++n);
+        System.out.print(n++);
+        System.out.print(n++);
+
+        char c = '1' + '3';
+        System.out.println(c);
+    }
+}
