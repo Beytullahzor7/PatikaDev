a67863ed5de482b34b04e5de95c3fe66780a470b
Beytullahzor7 <beytullahzor7@gmail.com>
2022-05-06 22:49:59 +0300

    Stream API Updated2

---
+++ src/FolksDev/javaOptional/PhoneBookCrawler.java
@@ -0,0 +1,40 @@
+package FolksDev.javaOptional;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class PhoneBookCrawler {
+
+    private PhoneBook phoneBook;
+
+    public PhoneBookCrawler(PhoneBook phoneBook) {
+        this.phoneBook = phoneBook;
+    }
+
+    public String findPhoneNumberByNameAndPunishIfNothingFound(String name) {
+        return phoneBook.findPhoneNumberByName(name)
+                .orElseThrow(() -> new IllegalArgumentException("No phone number found"));
+    }
+
+    public String findPhoneNumberByNameAndPrintPhoneBookIfNothingFound(String name) {
+        final StringBuilder result = new StringBuilder();
+
+        phoneBook.findPhoneNumberByName(name)
+                .ifPresentOrElse(result::append, () -> System.out.println(phoneBook.getPhoneBookEntries()));
+
+        return result.toString();
+    }
+
+    public String findPhoneNumberByNameOrNameByPhoneNumber(String name, String phoneNumber) {
+
+        return phoneBook.findPhoneNumberByName(name)
+                .orElse(phoneBook.findNameByPhoneNumber(phoneNumber)
+                        .orElse(phoneBook.findPhoneNumberByName("Jos de Vos")
+                                .orElse(null)));
+    }
+
+    public PhoneBook getPhoneBook() {
+        return phoneBook;
+    }
+}
+
